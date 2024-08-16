6962bca5e163bdd52d41df1c57c7734a48cc6194
bohdan.snopok <snopokbogdan3@gmail.com>
2024-08-16 23:00:31 +0300

    add search input and debounce hook

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -6,14 +6,15 @@ const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 console.log('BASE_URL:', BASE_URL);
 console.log('API_KEY:', API_KEY);
 
-export const getNews = async ({page_number = 1, page_size = 10, category}) => {
+export const getNews = async ({page_number = 1, page_size = 10, category, keywords}) => {
     try {
         const response = await axios.get(`${BASE_URL}search`, {
             params: {
                 apiKey: API_KEY,
                 page_number,
                 page_size,
-                category
+                category,
+                keywords
             },
         });
         return response.data;
