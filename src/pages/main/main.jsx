25d49bda4361e174048902ca5d099a69c90eefec
bohdan.snopok <snopokbogdan3@gmail.com>
2024-08-16 11:43:48 +0300

    add filtering by category

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,31 +1,52 @@
 import styles from './style.module.css';
 import NewsBanner from "../../components/newsBanner/newsBanner";
 import {useEffect, useState} from "react";
-import {getNews} from "../../api/apiNews";
+import {getCategories, getNews} from "../../api/apiNews";
 import NewsList from "../../components/NewsList/NewsList";
 import Skeleton from "../../components/Skeleton/Skeleton";
 import Pagination from "../../components/Pagination/Pagination";
+import Categories from "../../components/Categories/Categories";
 
 const Main = () => {
     const [news, setNews] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [currentPage, setCurrentPage] = useState(1)
+    const [categories, setCategories] = useState([])
+    const [selectedCategory, setSelectedCategory] = useState('All')
     const totalPage = 10
     const pageSize = 10
     const fetchNews = async (currentPage) => {
         try {
             setIsLoading(true)
-            const response = await getNews(currentPage, pageSize);
+            const response = await getNews({
+                page_number: currentPage,
+                page_size: pageSize,
+                category: selectedCategory === 'All' ? null : selectedCategory
+            });
             setNews(response.news);
             setIsLoading(false)
         } catch (error) {
             console.log("Error fetching news:", error);
         }
     };
+
+    const fetchCategories = async () => {
+        try {
+            const response = await getCategories();
+            setCategories(['All', ...response.categories]);
+        } catch (error) {
+            console.log("Error fetching news:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchCategories()
+    }, []);
+
     useEffect
     (() => {
         fetchNews(currentPage);
-    }, [currentPage]);
+    }, [currentPage, selectedCategory]);
 
     const handleNextPage = () => {
         if (currentPage < totalPage) {
@@ -44,6 +65,7 @@ const Main = () => {
     }
 
     return <main className={styles.main}>
+        <Categories categories={categories} selectedCategory={selectedCategory } setSelectedCategory={setSelectedCategory}/>
         {news.length > 0 && !isLoading ? <NewsBanner item={news[0]}/> : <Skeleton type={'banner'} count={1}/>}
 
         <Pagination handleNextPage={handleNextPage}
