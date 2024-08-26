ecb221494b55b298f8b5563808f732e952970e3d
bohdan.snopok <snopokbogdan3@gmail.com>
2024-08-26 18:45:35 +0300

    Збереження змін у NewsList.jsx

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -2,13 +2,11 @@ import styles from './style.module.css'
 import NewsItem from "../Newsitem/Newsitem";
 
 const NewsList = ({news}) => {
-    return (
-        <ul className={styles.list}>
+    return <ul className={styles.list}>
             {news.map(item => {
                 return <NewsItem key={item.id} item={item}/>
             })}
         </ul>
-    )
 };
 
 export default NewsList
